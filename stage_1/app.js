1c6f1169fcf6b6873038139788c672991a05c2a9
stephenobi720 <57940359+stephenobi720@users.noreply.github.com>
2023-09-08 18:26:11 +0100

    Update app.js

--- stage_1/app.js
+++ stage_1/app.js
@@ -2,23 +2,27 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/get_info', (req, res) => {
     const { slack_name, track } =req.query;
+
+    // for my name
+    const myName = slack_name || 'Stephen441';
+    const myTrack = track || 'backend'; 
 
     // to get the current day and time 
     const currentDayOfWeek = new Date().toLocaleDateString('en-US', { weekday: 'long'});
     const currentUtcTime = new Date().toISOString();
 
     // For github urls
-    const githubUrlFile = '';
-    const githubUrlRepo = '';
+    const githubUrlFile = 'https://github.com/stephenobi720/HNG_Projects/blob/main/stage_1/app.js';
+    const githubUrlRepo = 'https://github.com/stephenobi720/HNG_Projects/';
 
     // response
     const response = {
-        'Slack name': slack_name,
+        'Slack name': myName,
         'Current day of the week': currentDayOfWeek,
         'Current UTC time': currentUtcTime,
-        'Track': track,
+        'Track': myTrack,
         'Github File URL': githubUrlFile,
         'Github Repo URL': githubUrlRepo,
         'Status code': 200,
