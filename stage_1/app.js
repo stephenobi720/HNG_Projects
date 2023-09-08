e90a2b89667da2cdc7585b8519fd569353e6dd2c
stephenobi720 <57940359+stephenobi720@users.noreply.github.com>
2023-09-08 18:41:30 +0100

    Update app.js api

--- stage_1/app.js
+++ stage_1/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/get_info', (req, res) => {
+app.get('/api', (req, res) => {
     const { slack_name, track } =req.query;
 
     // for my name
